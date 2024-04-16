a25141af165cab33c999ce30a956f668f746921c
Sahargpm <104004230+Sahargpm@users.noreply.github.com>
2024-04-16 09:33:58 +0000

    background setting

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Form from '../component/Form'
 export default function ContactUs() {
   return (
-    <div>
+    <div  className="w-full bg-secondary" > 
       <Form></Form>
     </div>
   )
